30aa1e796579ba53da43ca7bc94433e937acf18b
Wayne Chen <wayen.chen@shoalter.com>
2021-08-19 14:42:35 +0800

    增加 rest 方法 PostXEncoded 處理x-www-form-urlencoded post的API

--- src/main/java/com/opendata/chatbot/util/RestTemplateUtil.java
+++ src/main/java/com/opendata/chatbot/util/RestTemplateUtil.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -21,6 +22,10 @@ public class RestTemplateUtil {
         return restTemplate.exchange(url, HttpMethod.POST,
                 new HttpEntity<>(json, headers),
                 String.class);
+    }
+
+    public static ResponseEntity<String> PostXEncoded(String url, HttpEntity<MultiValueMap<String, Object>> parameter){
+        return restTemplate.postForEntity(URI.create(url), parameter, String.class);
     }
 
     public static ResponseEntity<String> GetTemplate(String url, String json, HttpHeaders headers){
